003deb70d68e301fdd9f6499428c7e573f649c35
tengshuq <tengshuq@qq.com>
2018-01-03 11:47:57 +0800

    example

--- ChainFunction/ViewController.m
+++ ChainFunction/ViewController.m
@@ -18,13 +18,25 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     
-    UILabel *label = [UILabel new];
+    UILabel *test1 = [[UILabel alloc] init];
+    test1.frame = CGRectMake(20, 50, 100, 30);
+    test1.font = [UIFont systemFontOfSize:13];
+    test1.text = @"古今多少事";
+    test1.textColor = [UIColor whiteColor];
+    test1.backgroundColor = [UIColor lightGrayColor];
+    test1.textAlignment = NSTextAlignmentCenter;
+    test1.layer.cornerRadius = 3.f;
+    test1.layer.borderColor = [UIColor greenColor].CGColor;
+    test1.layer.borderWidth = 1.f;
+    [self.view addSubview:test1];
+    
+    UILabel *label = [[UILabel alloc] init];
     label.labelChain
     .font([UIFont systemFontOfSize:13])
     .text(@"链式作死")
     .textColor([UIColor greenColor])
     .viewMaker()
-    .frame(CGRectMake(20, 70, 100, 40))
+    .frame(CGRectMake(20, 100, 100, 40))
     .backgroundColor([UIColor whiteColor])
     .addToSuperView(self.view)
     .labelMaker()
